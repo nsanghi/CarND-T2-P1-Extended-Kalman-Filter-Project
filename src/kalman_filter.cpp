210f5cc3e17cec5143bb71c9e6ad643f0a27f938
Nimish Sanghi <nimish.sanghi@gmail.com>
2017-03-18 12:54:21 +0530

    updated Extended Kalman Equations

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
 #include "kalman_filter.h"
+#include <iostream>
+
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using namespace std;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -22,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +35,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +55,43 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  //recover state parametersto be between -pi and pi
+	float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
+  float c1 = sqrt(px*px+py*py);
+
+  VectorXd z_pred(3);
+  if(fabs(c1) < 0.000001){
+    z_pred << 0, 0, 0;
+  } else {
+    z_pred << c1, atan2(py, px), (px*vx+py*vy)/c1;
+  }
+  VectorXd y = z - z_pred;
+
+  //adjust phi in y to be between -pi and pi
+  const double pi = 3.14159265358979323846;
+  while (y(1) > pi || y(1) < -pi) {
+    if (y(1) > pi)
+      y(1) -= 2*pi;
+    else
+      y(1) += 2*pi;
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  //cout << "DEBUG" << endl;
+  //cout << "z_pred = " << z_pred << endl;
+  //cout << "y = " << y << endl;
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
